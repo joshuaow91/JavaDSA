e145068204aa0ed3edc91fe62accdc71ab8c6fe1
Joshua Ow <joshuaow@gmail.com>
2023-10-19 09:28:32 -0500

    Initial commit

---
+++ src/LinkedList/Constructor/LinkedList.java
@@ -0,0 +1,55 @@
+package LinkedList.Constructor;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public class Node{
+        int value;
+        Node next;
+
+        public Node (int value) {
+            this.value = value;
+        }
+
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+}
