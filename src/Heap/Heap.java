22f0bc2ea1ebfaf1a67b53d747a40a8adf51f80b
Joshua Ow <joshuaow@gmail.com>
2023-11-08 16:59:54 -0600

    das

---
+++ src/Heap/Heap.java
@@ -0,0 +1,91 @@
+package Heap;
+
+//Heap is a BST except the tree will always be complete and the root is always higher than it's descendants and so on. You can also
+// have duplicates.
+
+//min heap - min val at top
+// max heap - max val at top
+
+//implement a heap with an ArrayList<>
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    public void insert(int value) {
+        heap.add(value);
+        int current = heap.size() - 1;
+
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public Integer remove() {
+        if (heap.isEmpty()) return null;
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        int maxValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+
+        return maxValue;
+    }
+
+    public void sinkDown (int index) {
+        int maxIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+            if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+
+            if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+
+            if(maxIndex != index) {
+                swap(index, rightIndex);
+                index = maxIndex;
+            } else {
+                return;
+            }
+
+        }
+    }
+
+}
---
+++ src/rBST/rBST.java
@@ -0,0 +1,15 @@
+package rBST;
+
+import org.w3c.dom.Node;
+
+public class rBST {
+
+
+    private boolean rContains(Node currentNode, int value) {
+
+    }
+
+    private boolean rContains(int value) {
+
+    }
+}
